8b2f9569297219658dda9f401b71a2c8f8aabf1e
Moises Nissan <moisesns28@hotmail.com>
2021-08-24 12:54:19 -0500

    added seeds with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+puts 'Creating a user...'
+user = User.new(
+  email: "nacho@gmail.com",
+  password: "12345678"
+)
+user.save!
+
+puts 'Creating 15 fake monkeys...'
+15.times do
+  monkey = Monkey.new(
+    user: user,
+    name: Faker::FunnyName.name,
+    species: "Simian",
+    age: rand(1..20),
+    weight: rand(5..30),
+    price: rand(150..300),
+    address: Faker::Address.street_address,
+    lat: rand(5..10),
+    lng: rand(5..10)
+  )
+  monkey.save!
+end
+puts 'Finished!'
