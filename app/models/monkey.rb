00420bc53ce33517de6cac92d5c48c7e8512781b
eduardo valenzuela <valenzuelagarza@gmail.com>
2021-08-25 15:51:10 -0500

    Added dependent destroy to monkeys rentals

--- app/models/monkey.rb
+++ app/models/monkey.rb
@@ -1,6 +1,7 @@
 class Monkey < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  has_many :rentals, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   geocoded_by :address, latitude: :lat, longitude: :lng
